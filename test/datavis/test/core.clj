109320cc0c19783af3f6ba7000716126f7ba3391
Brian Rowe <briprowe@yahoo.com>
2011-08-29 21:39:25 -0400

    Stash

--- test/datavis/test/core.clj
+++ test/datavis/test/core.clj
@@ -1,6 +1,14 @@
 (ns datavis.test.core
   (:use [datavis.core])
-  (:use [clojure.test]))
+  (:use [clojure.test])
+  (:require [clojure.java [io :as io]]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(def *file1* nil)
+(def *file2* nil)
+(def *file3* nil)
+
+(deftest test-get-color
+  (with-open [file1 (read-data-file *file1*)
+              file2 (read-data-file *file2*)
+              file3 (read-data-file *file3*)]
+    ))
